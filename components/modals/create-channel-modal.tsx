dec3d8ddc1b8a937c76f5535aad41b6590a28390
Punith Moger <mogerpunith@gmail.com>
2025-05-24 00:11:51 +0530

    channel create

---
+++ components/modals/create-channel-modal.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
+import { ChannelType } from "@prisma/client";
+
+const fromSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "Server name is required.",
+    })
+    .max(20,{message:"Channel name cannot be this long."})
+    .refine((name) => name !== "general", {
+      message: "Channel name cannot be 'general' ",
+    }),
+
+  type: z.nativeEnum(ChannelType),
+});
+
+export const CreateChannelModal = () => {
+  const { isOpen, onClose, type } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "createChannel";
+
+  const form = useForm({
+    resolver: zodResolver(fromSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof fromSchema>) => {
+    try {
+      await axios.post("/api/servers", values);
+
+      form.reset();
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  return (
+    <div>
+      <Dialog open={isModalOpen} onOpenChange={handleClose}>
+        <DialogContent className="bg-white text-black p-0 overflow-hidden">
+          <DialogHeader className="pt-8 px-6">
+            <DialogTitle className="text-xl text-center font-bold">
+              Create a Channel
+            </DialogTitle>
+          </DialogHeader>
+          <Form {...form}>
+            <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="space-y-8 px-6">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="uppercase text-xs font-semibold text-zinc-500 dark:text-secondary/70">
+                        Channel Name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isLoading}
+                          className="bg- border-1 shadow-lg focus-visible:ring-0 focus-visible:ring-offset-0 text-black"
+                          placeholder="Enter Channel name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter className="bg-gray-100 py-4 px-6">
+                <Button variant="primary" disabled={isLoading}>
+                  Create
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
