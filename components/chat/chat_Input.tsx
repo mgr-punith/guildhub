f02f40f942221767b2c7649b8784c5a35e87b914
Punith Moger <mogerpunith@gmail.com>
2025-07-13 17:47:52 +0530

    message POST

--- components/chat/chat_Input.tsx
+++ components/chat/chat_Input.tsx
@@ -12,6 +12,7 @@ import { faFaceSmileBeam, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 interface ChatInputProps {
   apiUrl: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   query: Record<string, any>;
   name: string;
   type: "conversation" | "channel";
