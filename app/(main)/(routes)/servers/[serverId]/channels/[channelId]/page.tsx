969c2c3552eed4f4e7196a819606a4c0f63fe0c2
Punith Moger <mogerpunith@gmail.com>
2025-07-27 01:11:48 +0530

    deploying test3

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -10,10 +10,11 @@ import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
 
 interface ChannelIdPageProps {
-  params: {
+  params: Promise<{
     serverId: string;
     channelId: string;
-  };
+  }>;
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
